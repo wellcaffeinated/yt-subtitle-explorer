e38c6b0936450a65d5944cc6b2b01a4cfc89c140
Jasper Palfree <well.caffeinated@gmail.com>
2012-09-21 20:42:20 -0400

    Revert "Merge branch 'minutephysics'"
    This reverts commit ba2c9f6ca68db7374d4eabd04cac58f6f555ae6c, reversing
    changes made to 9ecf537f23a45e2193fd7448f0157198db3d87a1.

--- web/library/js/modules/userreport.js
+++
@@ -1,7 +0,0 @@
-define(['require'], function(require){
-	'use strict';
-
-	window._urq = window._urq || [];
-	window._urq.push(['initSite', 'ba4979bb-1321-4868-af0b-b95c1b174d49']);
-	require(['http://sdscdn.userreport.com/userreport.js']);
-});
