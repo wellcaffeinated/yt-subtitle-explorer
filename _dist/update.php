480022d5fe117f74d7586fca0094afcd6a803e2a
Jasper Palfree <well.caffeinated@gmail.com>
2012-10-08 14:00:47 -0400

    Updated update script

--- _dist/update.php
+++ _dist/update.php
@@ -1,11 +1,25 @@
 <?php
 
+use \Heartsentwined\FileSystemManager\FileSystemManager;
+
 return function($app){
 
     $base = __DIR__;
     
     if (is_dir($app['ytse.root'])){
 
-        copy($base.'/app', $app['ytse.root'].'/app');
+        // enable maintenance mode
+        $app['maintenance_mode']->enable();
+        
+        // copy app changes
+        FileSystemManager::rcopy($base.'/app', $app['ytse.root'].'/app');
+
+        // clear cache
+        $perms = fileperms($app['ytse.root'].'/cache');
+        FileSystemManager::rrmdir($app['ytse.root'].'/cache');
+        mkdir($app['ytse.root'].'/cache', $perms);
+
+        // disable maintenance mode
+        $app['maintenance_mode']->disable();
     }
 };
